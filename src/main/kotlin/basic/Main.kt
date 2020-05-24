ed3dafe81732b3de414399e5a3ed68909fbb87f1
haradayoshiaki <y.team.kobe@gmail.com>
2020-05-24 22:04:04 +0900

    リファクタ

--- src/main/kotlin/basic/Main.kt
+++ src/main/kotlin/basic/Main.kt
@@ -1,7 +1,7 @@
 package basic
 
 fun main() {
-    Container.register(SampleUsecase::class,SampleUsecase::class)
-    val driver = Container.get(SampleUsecase::class)
-    driver.execute()
+    Container.register(SampleUsecase::class)
+    val usecase = Container.get(SampleUsecase::class)
+    usecase.execute()
 }
--- src/main/kotlin/recursive/Main.kt
+++ src/main/kotlin/recursive/Main.kt
@@ -1,9 +1,11 @@
 package recursive
 
+import java.time.chrono.IsoChronology
+
 fun main() {
     Container.register(SampleDriver::class)
     Container.register(SampleGateway::class)
     Container.register(ISampleUsecase::class, SampleUsecase::class)
-    val usecase = Container.get<SampleUsecase>(SampleUsecase::class)
+    val usecase = Container.get(ISampleUsecase::class)
     usecase.execute()
 }
